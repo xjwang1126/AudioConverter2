b39f294d9e6cd8db5c634f1da894c90715a85f1a
xjwang1126 <xjwang1126@gmail.com>
2026-06-16 07:16:27 +0800

    display audio converter feature.

--- AudioTool/AudioToolApp.swift
+++ AudioTool/AudioToolApp.swift
@@ -2,13 +2,11 @@ import SwiftUI
 
 @main
 struct AudioToolApp: App {
-    @StateObject private var audioRecorder = AudioRecorder()
     @StateObject private var audioPlayer = AudioPlayer()
     
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(audioRecorder)
                 .environmentObject(audioPlayer)
         }
     }
